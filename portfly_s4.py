c702a86bb306dcd91adedcaec91fb6f154b4b1b3
xinlin-z <xinlin.zhang@hotmail.com>
2023-06-22 20:25:34 +0800

    tweaks

--- portfly_s4.py
+++ portfly_s4.py
@@ -13,90 +13,6 @@ log.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',
                 level=log.INFO)
 
 
-#def close_socket(s):
-#    """ socket no-raise-close interface """
-#    if s:
-#        try:
-#            s.shutdown(socket.SHUT_RDWR)
-#            s.close()
-#        except OSError:
-#            pass
-
-
-#def remove_list_ele(lst, ele):
-#    """ remove element from list with no raise """
-#    try:
-#        lst.remove(ele)
-#    except ValueError:
-#        pass
-
-
-SK_IO_CHUNK_LEN = 4096
-MAX_STREAM_ID   = 0xFFFFFFFF
-
-
-#def recv_sk_nonblock_forever(sk):
-#    """ socket nonblocking recv generator, last forever """
-#    data = b''
-#    while True:
-#        try:
-#            _d = sk.recv(SK_IO_CHUNK_LEN)
-#            if len(_d) == 0:
-#                raise ConnectionError('recv_sk_nonblock_forever recv 0')
-#            data += _d
-#            while (dlen:=len(data)) > 4:
-#                mlen = int.from_bytes(data[:4], 'little')
-#                if dlen >= mlen:
-#                    yield int.from_bytes(data[4:8],'big'), data[8:mlen]
-#                    data = data[mlen:]
-#                else:
-#                    break
-#        except BlockingIOError:
-#            yield None, b''
-
-
-#def recv_sk_nonblock(sk):
-#    """ socket nonblocking recv generator, one shot """
-#    while True:
-#        try:
-#            yield sk.recv(SK_IO_CHUNK_LEN)
-#        except BlockingIOError:
-#            return
-
-
-#def send_sk_nonblock_forever(sk):
-#    """ socket nonblocking send generator, last forever """
-#    data = b''
-#    while True:
-#        bmsg, sid = yield
-#        if bmsg is not None:
-#            data += (len(bmsg)+8).to_bytes(4,'little') \
-#                            + sid.to_bytes(4,'big') \
-#                            + bmsg
-#        try:
-#            while True:
-#                if len(data) == 0:
-#                    break
-#                if (i:=sk.send(data[:SK_IO_CHUNK_LEN])) == -1:
-#                    raise ConnectionError('send_sk_nonblock_forever send -1')
-#                data = data[i:]
-#        except BlockingIOError:
-#            continue
-
-
-#def send_sk_nonblock(sk_data):
-#    sk, data = sk_data
-#    try:
-#        while True:
-#            if (i:=sk.send(data[:SK_IO_CHUNK_LEN])) == -1:
-#                raise ConnectionError('send_sk_nonblock send -1')
-#            data = sk_data[1] = data[i:]
-#            if len(data) == 0:
-#                break
-#    except BlockingIOError:
-#        pass
-
-
 SK_IO_CHUNK_LEN = 4096
 MAX_STREAM_ID   = 0xFFFFFFFF
 
@@ -112,6 +28,20 @@ class trafix():
                 sk.shutdown(socket.SHUT_RDWR)
                 sk.close()
             except OSError:
+                pass
+
+    def close_remove(self, k, s=None):
+        _s, _ = self.sdict.pop(k, (None,None))
+        if s:
+            assert s is _s
+        else:
+            s = _s
+        if s:
+            self.kdict.pop(s, None)
+            trafix.close_socket(s)
+            try:
+                self.sread.remove(s)
+            except ValueError:
                 pass
 
     @staticmethod
@@ -175,21 +105,6 @@ class trafix():
         except BlockingIOError:
             pass
 
-    def close_remove(self, k, s=None):
-        _s, _ = self.sdict.pop(k, (None,None))
-        if s:
-            assert s is _s
-        else:
-            s = _s
-        if s:
-            self.kdict.pop(s, None)
-            try:
-                s.shutdown(socket.SHUT_RDWR)
-                s.close()
-                self.sread.remove(s)
-            except Exception:
-                pass
-
     def __init__(self, sk, pserv, port):
         # no need to set pserv to nonblocking
         self.pserv = pserv
@@ -208,7 +123,8 @@ class trafix():
     def go(self, port):
         while True:
             assert len(self.sdict) == len(self.kdict)
-            self.sread, _, _ = select.select([self.pserv,self.sk]+list(self.kdict.keys()),[],[],1)
+            selist = [self.pserv, self.sk] + list(self.kdict.keys())
+            self.sread, _, _ = select.select(selist,[],[],1)
             try:
                 self.gen_send.send((None,0))
                 if len(self.sread) == 0:
@@ -280,102 +196,8 @@ class trafix():
         trafix.close_socket(self.pserv)
         trafix.close_socket(self.sk)
         log.warning('[%d] closed', port)
-        
 
 
-def portfly_pserv(sk, pserv, port):
-    # set socket nonblocking, but pserv is not needed
-    sk.setblocking(False)
-    gen_recv = recv_sk_nonblock_forever(sk)
-    gen_send = send_sk_nonblock_forever(sk)
-    next(gen_send)
-
-    sid = 1       # sid, stream id, also called k
-    sdict = {}    # sid --> socket
-    kdict = {}    # socket --> sid
-    while True:
-        assert len(sdict) == len(kdict)
-        rs, _, _ = select.select([pserv,sk]+list(kdict.keys()),[],[],1)
-        try:
-            gen_send.send((None,0))
-            if len(rs) == 0:
-                continue
-            # new connections
-            if pserv in rs:
-                conn, addr = pserv.accept()
-                gen_send.send((mngt_prefix+b'gogogo',sid))
-                log.info('[%d] accept %s, sid %d', port, str(addr), sid)
-                conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
-                conn.setblocking(False)   # set nonblocking
-                sdict[sid] = [conn, b'']  # sid -> [socket,sending buffer]
-                kdict[conn] = sid
-                sid = sid+1 if sid!=MAX_STREAM_ID else 1
-                rs.remove(pserv)
-            # recv from client
-            if sk in rs:
-                while True:
-                    k, bmsg = next(gen_recv)
-                    if k:
-                        # connection die
-                        if (bmsg == mngt_prefix+b'sodie' and 
-                                k in sdict.keys()):
-                            log.info('[%d] close sid %d by client', port, k)
-                            s, _ = sdict.pop(k, (None,None))
-                            close_socket(s)
-                            remove_list_ele(rs, s)
-                            if s: kdict.pop(s, None)
-                        # heartbeat
-                        elif bmsg == hb_bmsg:
-                            log.info('[%d] recv & send heartbeat (sid=%d)', port, k)
-                            gen_send.send((hb_bmsg,k))
-                        # data
-                        else:
-                            try:
-                                if k in sdict.keys():
-                                    sdict[k][1] += bmsg
-                                    send_sk_nonblock(sdict[k])
-                            except OSError:
-                                log.info('[%d] sid %d is closed by exception',
-                                                                        port, k)
-                                gen_send.send((mngt_prefix+b'sodie',k))
-                                s, _ = sdict.pop(k, (None,None))
-                                close_socket(s)
-                                remove_list_ele(rs, s)
-                                if s: kdict.pop(s, None)
-                    else:
-                        break
-                rs.remove(sk)
-            # recv from connections
-            for s in rs:
-                k = kdict[s]
-                gen_data = recv_sk_nonblock(s)
-                while True:
-                    try:
-                        if (data:=next(gen_data)) == b'':
-                            raise OSError
-                    except OSError:
-                        log.info('[%d] sid %d is donw while recv', port, k)
-                        gen_send.send((mngt_prefix+b'sodie',k))
-                        sdict.pop(k, (None,None))
-                        close_socket(s)
-                        kdict.pop(s, None)
-                        break
-                    except StopIteration:
-                        break
-                    gen_send.send((data,k))  # send data
-        except Exception as e:
-            log.error('exception [%d]: %s', port, str(e))
-            log.exception(e)
-            for s,_ in sdict.values():
-                close_socket(s)
-            break
-    # while end
-    close_socket(pserv)
-    close_socket(sk)
-    log.warning('[%d] closed', port)
-
-
-# usage: python3 report_serv.py <listen_port>
 if __name__ == '__main__':
     addr = ('', int(sys.argv[1].strip()))
     serv = socket.create_server(('', int(sys.argv[1].strip())))
@@ -402,7 +224,6 @@ if __name__ == '__main__':
                 threading.Thread(target=trafix,
                                  args=(so,tserv,port),
                                  daemon=True).start()
-                #th.start()
             else:
                 raise ValueError('magic bmsg error')
         except Exception as e:
